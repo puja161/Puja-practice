ccee2baa16deb73463caf9bf88244ec45457221c
rainapreete143 <rainapreete143@gmail.com>
2014-10-10 16:11:40 +0530

    merging
    test

--- template/default/controller/index.html.php
+++ template/default/controller/index.html.php
@@ -4,7 +4,7 @@ echo "Thought Of The Day preeteeee";
 /* code added by puja*/
 echo "hello";
 /* code added by preete*/
-echo "hello buddy";
+echo "hello buddy preete";
 ?></h2>
 {foreach from=$aUsers item=aUser}
 	<h1>{$aUser.title}</h1>
